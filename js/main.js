9eedbd0bf25b0822704d8227b209cae5a1c71c5a
Gerardo Nille <gerardonille@gmail.com>
2016-08-30 02:03:40 -0500

    Done, en espera a revision

--- js/main.js
+++ js/main.js
@@ -7,3 +7,24 @@ $(document).ready(function() {
 function slide_section(section) {
     $("#" + section).slideToggle();
 }
+function slide_cirugia_pediatrica(section) {
+	$(".ribbon").slideToggle();
+    $(".right-cirugia").slideToggle();
+    $(".icon").slideToggle();
+    $(".contacto-50").slideToggle();
+    $(".title-background").slideToggle();
+
+    if ($("#" + section).is(":visible")) { 
+    $(".destroyme").each(function() {
+       $(this).remove();
+    });
+    $('.cirugias').append('<style class = "destroyme">.cirugias::before{width : 100%;}</style>');
+}
+else{
+    $(".destroyme").each(function() {
+       $(this).remove();
+    });
+    $('.cirugias').append('<style class = "destroyme">.cirugias::before{width : 50%;}</style>');
+}
+    $("#" + section).slideToggle();
+}
