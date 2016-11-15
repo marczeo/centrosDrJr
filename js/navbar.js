27e3020542ab3bf924957c654d65cb299eedc2b9
Marco Gómez <marco.gomez@outlook.com>
2016-11-15 14:46:08 -0600

    navbar

--- js/navbar.js
+++ js/navbar.js
@@ -3,13 +3,39 @@ jQuery(document).ready(function(){
 	var menu 	= jQuery('.menu');
 	var nav = jQuery(".navbar");
 	var ele = jQuery(".navbar-options");
+	var navDiv = jQuery(".navbar-fixed");
+	var max_mobile_mediaquery = 460;
+	var mq_mobile = window.matchMedia( "(min-width: "+max_mobile_mediaquery+"px)" );
+	var max_ipad_mediaquery = 800;
+	var mq = window.matchMedia( "(min-width: "+max_ipad_mediaquery+"px)" );
 
 	var divHeight;
 	var obj = document.getElementById('limit');
 
 	jQuery(touch).on('click', function(e) {
-		e.preventDefault();
-		ele.slideToggle();
+		e.preventDefault();		
+		ele.slideToggle('fast',function(){
+			if (mq_mobile.matches) {}
+			else{
+				if($('.navbar-options').is(":hidden"))
+				{
+					navDiv.css('width', '30%');
+				}
+				else{
+					navDiv.css('width', '50%');
+				}
+			}
+			if (mq.matches) {}
+			else{
+				if($('.navbar-options').is(":hidden"))
+				{
+					navDiv.css('width', '30%');
+				}
+				else{
+					navDiv.css('width', '50%');
+				}
+			}
+		});		
 	});
 
 	/*jQuery(window).resize(function(){
@@ -23,8 +49,18 @@ jQuery(document).ready(function(){
     jQuery(window).scroll(function () {
         if (jQuery(this).scrollTop() > obj.offsetHeight) {
             nav.addClass("navbar-show");
-        } else {
-            nav.removeClass("navbar-show");
+        } else {            
+            if (mq_mobile.matches) {
+            	nav.removeClass("navbar-show");
+	        } else {
+	          nav.addClass("navbar-show");
+	        }
+	        if (mq.matches) {
+            	nav.removeClass("navbar-show");
+	        } else {
+	          nav.addClass("navbar-show");
+	        }
         }
     });
+
 });
